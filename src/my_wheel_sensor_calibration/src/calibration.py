1da226b3968b6426cfe1347435f1d6d9f9b2159c
Aljoscha Frey <aljoscha.fry@gmail.com>
2019-11-25 13:24:23 +0100

    Finish implementing ü6:
    - Add delay before publishing anything
    - Send multiple messages per instruction
    - Calculate distance only when measuring/driving
    - Insert real Tick message format and handle them accordingly

--- src/my_wheel_sensor_calibration/src/calibration.py
+++ src/my_wheel_sensor_calibration/src/calibration.py
@@ -2,48 +2,61 @@
 
 import rospy
 from nav_msgs.msg import Odometry
-from autominy_msgs.msg import SteeringFeedback, NormalizedSpeedCommand, NormalizedSteeringCommand
+from autominy_msgs.msg import SteeringFeedback, NormalizedSpeedCommand, NormalizedSteeringCommand, Tick
 
 import math
 
 car_gps_id = 9  # for 129
+car_gps_id = 11  # for 121
 #calibrate_for = 1.0  # left
-#calibrate_for = 0.0  # straight
-calibrate_for = -1.0  # right
+calibrate_for = 0.0  # straight
+#calibrate_for = -1.0  # right
 
 distance_travelled = 0
 last_position = (-1, -1)
 
 count_ticks = False
+count_distance = False
 ticks = 0
 
 
+def publish(publisher, command):
+    for i in range(10):
+        publisher.publish(command)
+        rospy.sleep(0.1)
+
+
 def save_position(odometry):
-    global last_position, distance_travelled
+    global last_position, count_distance, distance_travelled
     current = (odometry.pose.pose.position.x, odometry.pose.pose.position.y)
-    if last_position != (-1, -1):
-        distance_travelled += math.sqrt(abs(last_position[0]-current[0])**2 + abs(last_position[0]-current[0])**2)
+    if last_position != (-1, -1) and count_distance:
+        delta = math.sqrt(abs(last_position[0]-current[0])**2 + abs(last_position[0]-current[0])**2)
+        if delta > 0.001:
+            distance_travelled += delta
     last_position = current
 
 
-def save_ticks(data):
+def save_ticks(tick):
     global count_ticks, ticks
     if count_ticks:
-        ticks += 1
+        ticks += tick.value
 
 
 def main():
-    global count_ticks, distance_travelled
-    steering.publish(NormalizedSteeringCommand(value=calibrate_for))
+    global count_ticks, count_distance, distance_travelled
+    rospy.sleep(1)  # wait for node to fully initialize
+    publish(steering, NormalizedSteeringCommand(value=calibrate_for))
+    print "Calibrator started, waiting for gps signal..."
     while last_position == (-1, -1):
         pass
-    count_ticks = True
-    speed.publish(NormalizedSpeedCommand(value=.2))
-    rospy.sleep(5)  # drive approximately 1 meter
-    count_ticks = False
-    speed.publish(NormalizedSpeedCommand(value=0))
+    publish(speed, NormalizedSpeedCommand(value=0.1))
+    count_ticks = count_distance = True
+    rospy.sleep(6)
+    count_ticks = count_distance = False
+    publish(speed, NormalizedSpeedCommand(value=0.0))
 
-    print(distance_travelled/ticks)
+    print "Finished. Measured distance:", distance_travelled, "; measured ticks:", ticks
+    print "Calculated distance tick ratio: ", distance_travelled/ticks
 
 
 if __name__ == '__main__':
@@ -51,6 +64,6 @@ if __name__ == '__main__':
     rospy.Subscriber("/communication/gps/" + str(car_gps_id), Odometry, save_position)
     speed = rospy.Publisher("/actuators/speed_normalized", NormalizedSpeedCommand, queue_size=10)
     steering = rospy.Publisher("/actuators/steering_normalized", NormalizedSteeringCommand, queue_size=10)
-    rospy.Subscriber("/sensors/arduino/ticks", None, save_ticks)
+    rospy.Subscriber("/sensors/arduino/ticks", Tick, save_ticks)
 
     main()
